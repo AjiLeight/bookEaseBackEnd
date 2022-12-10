b3e5dd5d99d25d268939d98f106a9d08b4d86ae7
Ajith <ajith.pgm89@gmail.com>
2022-12-10 13:54:26 +0530

    Project initialised with database connectivity

---
+++ src/main/java/com/nssproject/bookease/BookEaseApplication.java
@@ -0,0 +1,13 @@
+package com.nssproject.bookease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nssproject/bookease/BookEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nssproject.bookease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
