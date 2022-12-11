386ec0ea80192534f2aaa0b6e4998013255f60de
Ajith <ajith.pgm89@gmail.com>
2022-12-11 13:45:20 +0530

    save book api

--- src/main/java/com/nssproject/bookease/controller/BookController.java
+++ src/main/java/com/nssproject/bookease/controller/BookController.java
@@ -1,4 +1,26 @@
 package com.nssproject.bookease.controller;
 
+import com.nssproject.bookease.entity.Book;
+import com.nssproject.bookease.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/api/save-book")
+    public ResponseEntity<Book> saveBook(@RequestBody Book book){
+        return new ResponseEntity<Book>(bookService.saveBook(book), HttpStatus.OK);
+    }
 }
--- src/main/java/com/nssproject/bookease/serviceImpl/BookServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.nssproject.bookease.serviceImpl;
-
-public class BookServiceImpl {
-}
