d56473bfb9dca7a468ee649a6464e1007d324b3c
Ajith <ajith.pgm89@gmail.com>
2023-02-20 15:08:48 +0530

    Changed expiration time and configuration

--- src/main/java/com/nssproject/bookease/constants/SecurityConstants.java
+++ src/main/java/com/nssproject/bookease/constants/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.nssproject.bookease.constants;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 10000000;
     public static final String SECRET_KEY = "secret";
 }
